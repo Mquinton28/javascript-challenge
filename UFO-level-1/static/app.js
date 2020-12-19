289d7145700146c56d53b94b28e8197b3331f11b
Maria Quinton <maria.quinton28@gmail.com>
2020-12-19 11:41:06 -0500

    Working through step one

---
+++ UFO-level-1/static/app.js
@@ -0,0 +1,27 @@
+// from data.js
+var tableData = data;
+
+//Get references
+var tbody = d3.select('tbody')
+var columns = ["datetime", "city", "state", "country", "shape", "comments"]
+var inputData = d3.select('#datetime')
+var button = d3.select('filter-btn')
+
+function findData(data) {
+    tbody.text('')
+    data.forEach(function(sighting){
+        input_tr = tbody.append('tr')
+        Object.entries(sighting).forEach(function([key, vlaue]){
+            input_td = input_tr.append('td').text(value)
+        })
+    })
+}
+
+findData(tableData)
+
+// filter data to find date
+function findDate() {
+    d3.event.preventDefault();
+    console.log(inputData.property('value'));var updatedTable = tableData.filter(sighting => sighting.datetime===inputData.property('value'))
+    findData(updatedTable);
+}
