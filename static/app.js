d48a3308b1e7b403405a40359a9f3dcca24f2afa
Maria Quinton <mariaquinton@Marias-MBP.lan1>
2021-01-25 19:42:10 -0600

    updates

--- static/app.js
+++ static/app.js
@@ -1,140 +1,61 @@
 // from data.js
 var tableData = data;
 
-//Get references table
-var tbody = d3.select('#target-table-display');
-console.log(tableData);
+// select tbody frmo index.html
 
-var button = d3.select('#filter-btn-date');
-var inputField = d3.select('#datetime');
+tbody = d3.select("tbody")
 
-varcityButton = d3.select('#filter-btn-city');
-varcityInput = d3.select('#cityname');
+// use Object.entries to get key, value data inside of the table
 
-varstatesubmitButtion = d3.select('#filter-btn-state');
-var stateInput = d3.select('#statename');
+// and loop through them to add to the table in html
 
-var countrysubmitButtion = d3.select('#filter-btn-country');
-var countryInput = d3.select('#countryname');
+function displayData(data) {
 
-var shapesubmitButtion = d3.select('#filter-btn-shape');
-var shapesInput = d3.select('#shapename');
+    tbody.text("")
 
+    data.forEach(function (sighting) {
 
+        new_tr = tbody.append("tr")
 
-//Arrow function to append the tableData
-data.forEach(ufo => {
-    var row = tbody.append('tr');
-    Object.values(ufo).forEach(([key,value]) => {
-        var cell = row.append('td');
-        cell.text(value);
-    });
-});
+        Object.entries(sighting).forEach(function ([key, value]) {
 
-button.on('click', function() {
-    tbody.html('');
+            new_td = new_tr.append("td").text(value)
 
-    // prevent page from refreshing
+        })
+
+    })
+}
+
+displayData(tableData)
+
+//select the user's input and the filter button
+
+var dateInputText = d3.select("#datetime")
+
+var button = d3.select("filter-btn")
+
+// filter data with user input date
+
+function clickSelect() {
+
+    //don't refresh the page!
+
     d3.event.preventDefault();
 
-    var inputData = d3.select('#datetime');
+    //print the value that was input
 
-    var inputValue = inputData.property('value');
+    console.log(dateInputText.property("value"));
 
-    console.log(inputValue);
+    //create a new table showing only the filterd data
 
-    var inputArray = data.filter(one => one.datetime === inputElement);
-    
-});
+    var new_table = tableData.filter(sighting => sighting.datetime === dateInputText.property("value"))
 
-inputArray.forEach((selection) => {
-    var row = tbody.append('tr');
-    Object.entries(selection).forEach(([key,value]) => {
-        var cell = row.append('td');
-        cell.text(value);
-    });
-});
+    //display the new table
 
-// Level 2
-cityButton.on('click', function() {
-    tbody.html('');
-    d3.event.preventDefault();
-    console.log('You just clicked the filter by city button');
-    var cityInput = cityinputField.property('value');
-    console.log(cityInput);
-    var cityinputArray = data.filter(two => two.city === cityInput);
-    console.log(cityInputArray);
-    
-    cityinputArray.forEach((selects) => {
-    var brow = tbody.append('tr');
-    Object.entries(selects).forEach([key,value]) => {
-        var bcell = brow.append('td');
-        bcell.text(value);
-    }
+    displayData(new_table);
 
-    });
-});
+}
 
-stateButton.on('click', function() {
-    tbody.html('');
-    d3.event.preventDefault();
-    console.log('You just clicked the filter by state button');
+// event listener to handle change and click
 
-    var stateInput = stateinputField.property('value');
-    console.log(stateInput);
-    var stateinputArray = data.filter(three => three.state === stateInput);
-    console.log(stateinputArray);
-
-    // Display the html file and append it
-    stateinputArray.forEach((selectss) => {
-        var crow = tbody.append('tr');
-        Object.entries(selectss).forEach(([key,value]) => {
-            var ccell = crow.append('td');
-            ccell.text('value');
-        });
-    });
-});
-
-countryButton.on('click', function() {
-    tbody.html('');
-    d3.event.preventDefault();
-    console.log('You just clicked the filter by country button');
-
-    var countryInput = countryinputField.property('value');
-    console.log(countryInput);
-    var countryinputArray = data.filter(four => four.country === countryInput);
-    console.log(countryinputArray);
-
-    // Display the html file and append it
-    countryinputArray.forEach((selectss) => {
-        var drow = tbody.append('tr');
-        Object.entries(selectss).forEach(([key,value]) => {
-            var dcell = drow.append('td');
-            dcell.text('value');
-        });
-    });
-});
-
-shapeButton.on('click', function() {
-    tbody.html('');
-    d3.event.preventDefault();
-    console.log('You just clicked the filter by sshape button');
-
-    var shapeInput = shapeinputField.property('value');
-    console.log(stateInput);
-    var shapeinputArray = data.filter(five => five.shape === shapeInput);
-    console.log(shapeinputArray);
-
-    // Display the html file and append it
-    shapeinputArray.forEach((selectss) => {
-        var erow = tbody.append('tr');
-        Object.entries(selectss).forEach(([key,value]) => {
-            var ecell = erow.append('td');
-            ecell.text('value');
-        });
-    });
-});
-
-//event listener
-inputData.on('change', changeHandler)
-button.on('click', changeHandler)
+dateInputText.on("change", clickSelect)
